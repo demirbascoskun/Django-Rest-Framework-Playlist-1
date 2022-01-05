b8e5cea12b5e4cb8d052217da0d7e973b453f9ef
Harun Demirbas <sozalotomotivistanbul@gmail.com>
2022-01-06 02:42:17 +0300

    Lesson 1 Launcing our project and creating model

---
+++ news/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Article(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    main_text = models.TextField()
+    published_time = models.DateField()
+    is_active = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
